19eb362c9e85b6d6a99b3e70a7d98d388ad2b4e5
sampriya <monjury@gmail.com>
2015-08-04 00:04:30 -0700

    Example 1

---
+++ Chapter15/src/example/runnable/MyRunnable.java
@@ -0,0 +1,37 @@
+package example.runnable;
+
+public class MyRunnable implements Runnable {
+
+	public int count = 0;
+
+	@Override
+	public void run() {
+		System.out.println("Thread start");
+		try {
+			while (count < 5) {
+				Thread.sleep(50);
+				count++;
+			}
+		}
+		catch (InterruptedException e) {
+			System.out.println("Thread interrupted");
+		}
+		System.out.println("Thread end");
+	}
+
+	public static void main(String[] args) {
+		MyRunnable r = new MyRunnable();
+		Thread t = new Thread(r);
+		t.start();
+		while (r.count != 5) {
+			try {
+				Thread.sleep(25);
+			}
+			catch (InterruptedException e) {
+				System.out.println("Main Thread interrupted");
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
